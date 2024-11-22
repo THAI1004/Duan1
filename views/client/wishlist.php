f565e339356c0bcff30a1516b6b54d40aa261b2d
thaiph <thaiphph52804@gmail.com>
2024-11-22 23:43:45 +0700

    Wishlist

---
+++ views/client/wishlist.php
@@ -0,0 +1,73 @@
+
+   <main>
+        <!-- breadcrumb area start -->
+        <div class="breadcrumb-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="breadcrumb-wrap">
+                            <nav aria-label="breadcrumb">
+                                <ul class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.html"><i class="fa fa-home"></i></a></li>
+                                    <li class="breadcrumb-item"><a href="shop.html">shop</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">wishlist</li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- breadcrumb area end -->
+
+        <!-- wishlist main wrapper start -->
+        <div class="wishlist-main-wrapper section-padding">
+            <div class="container">
+                <!-- Wishlist Page Content Start -->
+                <div class="section-bg-color">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <!-- Wishlist Table Area -->
+                            <div class="cart-table table-responsive">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th class="pro-thumbnail">Thumbnail</th>
+                                            <th class="pro-title">Product</th>
+                                            <th class="pro-price">Price</th>
+                                            <th class="pro-quantity">Stock Status</th>
+                                            <th class="pro-subtotal">Add to Cart</th>
+                                            <th class="pro-remove">Remove</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach($wishlist as $row){?>
+                                        <tr>
+                                            <td class="pro-thumbnail"><a href="#"><img class="img-fluid" src="<?= $row["product_image"]?>" alt="Product" /></a></td>
+                                            <td class="pro-title"><a href="#"><?= $row["product_name"]?></a></td>
+                                            <td class="pro-price"><span><?= $row["product_price"]?></span></td>
+                                            <td class="pro-quantity"><span class="text-success">In Stock</span></td>
+                                            <td class="pro-subtotal"><a href="cart.html" class="btn btn-sqr">Add to
+                                                    Cart</a></td>
+                                            <td class="pro-remove"><a href="?act=deleteWishlist&id=<?=$row["product_id"]?>"><i class="fa fa-trash-o"></i></a></td>
+                                        </tr>
+                                        <?php }?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Wishlist Page Content End -->
+            </div>
+        </div>
+        <!-- wishlist main wrapper end -->
+    </main>
+
+    <!-- Scroll to top start -->
+    <div class="scroll-top not-visible">
+        <i class="fa fa-angle-up"></i>
+    </div>
+<?php
+include "./include/footerClient.php";
+?>
