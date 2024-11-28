b0aaadf0c0bec77a12e15f8de2af688b28fd340d
nguyenthonghuy16012003 <huyten26@gmail.com>
2024-11-28 23:56:02 +0700

    Done slider

--- views/client/contactUs.php
+++ views/client/contactUs.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,122 +24,94 @@
     <!-- main style css -->
     <link rel="stylesheet" href="./corano/assets/css/style.css">
 </head>
+
 <body>
-    
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<main>
-        <!-- breadcrumb area start -->
-        <div class="breadcrumb-area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="breadcrumb-wrap">
-                            <nav aria-label="breadcrumb">
-                                <ul class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="?act=homeClient"><i class="fa fa-home"></i></a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">contact us</li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-      
-        <!-- contact area start -->
-        <div class="contact-area section-padding mt-5 pt-0">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="contact-message">
-                            <h4 class="contact-title">Tell Us Your Project</h4>
-                            <form id="contact-form" action="https://whizthemes.com/mail-php/genger/mail.php" method="post" class="contact-form">
-                                <div class="row">
-                                    <div class="col-lg-6 col-md-6 col-sm-6">
-                                        <input name="first_name" placeholder="Name *" type="text" required>
-                                    </div>
-                                    <div class="col-lg-6 col-md-6 col-sm-6">
-                                        <input name="phone" placeholder="Phone *" type="text" required>
-                                    </div>
-                                    <div class="col-lg-6 col-md-6 col-sm-6">
-                                        <input name="email_address" placeholder="Email *" type="text" required>
-                                    </div>
-                                    <div class="col-lg-6 col-md-6 col-sm-6">
-                                        <input name="contact_subject" placeholder="Subject *" type="text">
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="contact2-textarea text-center">
-                                            <textarea placeholder="Message *" name="message" class="form-control2" required=""></textarea>
-                                        </div>
-                                        <div class="contact-btn">
-                                            <button class="btn btn-sqr" type="submit">Send Message</button>
-                                        </div>
-                                    </div>
-                                    <div class="col-12 d-flex justify-content-center">
-                                        <p class="form-messege"></p>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="contact-info">
-                            <h4 class="contact-title">Contact Us</h4>
-                            <p>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum
-                                est notare quam littera gothica, quam nunc putamus parum claram anteposuerit litterarum
-                                formas human.</p>
-                            <ul>
-                                <li><i class="fa fa-fax"></i> Address : No 40 Baria Sreet 133/2 NewYork City</li>
-                                <li><i class="fa fa-phone"></i> E-mail: info@yourdomain.com</li>
-                                <li><i class="fa fa-envelope-o"></i> +88013245657</li>
-                            </ul>
-                            <div class="working-time">
-                                <h6>Working Hours</h6>
-                                <p><span>Monday – Saturday:</span>08AM – 22PM</p>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+
+    <body>
+        <main>
+            <!-- breadcrumb area start -->
+            <div class="breadcrumb-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="breadcrumb-wrap">
+                                <nav aria-label="breadcrumb">
+                                    <ul class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="?act=homeClient"><i class="fa fa-home"></i></a></li>
+                                        <li class="breadcrumb-item active" aria-current="page">contact us</li>
+                                    </ul>
+                                </nav>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <!-- contact area end -->
-    </main>
-    <script src="./corano/assets/js/vendor/modernizr-3.6.0.min.js"></script>
-    <!-- jQuery JS -->
-    <script src="./corano/assets/js/vendor/jquery-3.6.0.min.js"></script>
-    <!-- Bootstrap JS -->
-    <script src="./corano/assets/js/vendor/bootstrap.bundle.min.js"></script>
-    <!-- slick Slider JS -->
-    <script src="./corano/assets/js/plugins/slick.min.js"></script>
-    <!-- Countdown JS -->
-    <script src="./corano/assets/js/plugins/countdown.min.js"></script>
-    <!-- Nice Select JS -->
-    <script src="./corano/assets/js/plugins/nice-select.min.js"></script>
-    <!-- jquery UI JS -->
-    <script src="./corano/assets/js/plugins/jqueryui.min.js"></script>
-    <!-- Image zoom JS -->
-    <script src="./corano/assets/js/plugins/image-zoom.min.js"></script>
-    <!-- Images loaded JS -->
-    <script src="./corano/assets/js/plugins/imagesloaded.pkgd.min.js"></script>
-    <!-- mail-chimp active js -->
-    <script src="./corano/assets/js/plugins/ajaxchimp.js"></script>
-    <!-- contact form dynamic js -->
-    <script src="./corano/assets/js/plugins/ajax-mail.js"></script>
-    <!-- google map api -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCfmCVTjRI007pC1Yk2o2d_EhgkjTsFVN8"></script>
-    <!-- google map active js -->
-    <script src="./corano/assets/js/plugins/google-map.js"></script>
-    <!-- Main JS -->
-    <script src="./corano/assets/js/main.js"></script>
+
+            <!-- contact area start -->
+            <div class="contact-area section-padding mt-5 pt-0">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="contact-info">
+                                <h4 class="contact-title">Liên hệ với chúng tôi :</h4>
+                                <p>Chúng tôi luôn sẵn sàng lắng nghe và hỗ trợ bạn. Nếu bạn có bất kỳ câu hỏi, góp ý, hoặc yêu cầu nào, đừng ngần ngại liên hệ với chúng tôi. Hãy sử dụng biểu mẫu bên dưới, gọi điện hoặc gửi email – đội ngũ của chúng tôi sẽ phản hồi bạn trong thời gian sớm nhất.
+
+                                    Hãy  kết nối với chúng tôi, cùng xây dựng những trải nghiệm tốt hơn !</p>
+                                <ul>
+                                    <li><i class="fa fa-fax"></i> Địa chỉ : Tòa nhà FPT Polytechnic, Phố Trịnh Văn Bô, Nam Từ Liêm, Hà Nội.</li>
+                                    <li><i class="fa fa-phone"></i> Điện thoại: (024) 7300 1955</li>
+                                    <li><i class="fa fa-envelope-o"></i> E-mail: caodang@fpt.edu.vn</li>
+                                </ul>
+                                <div class="working-time">
+                                    <h6>Thời gian làm việc:</h6>
+                                    <p><span>Thứ 2 – Thứ 7:</span>08AM – 22PM</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- contact area end -->
+        </main>
+        <script src="./corano/assets/js/vendor/modernizr-3.6.0.min.js"></script>
+        <!-- jQuery JS -->
+        <script src="./corano/assets/js/vendor/jquery-3.6.0.min.js"></script>
+        <!-- Bootstrap JS -->
+        <script src="./corano/assets/js/vendor/bootstrap.bundle.min.js"></script>
+        <!-- slick Slider JS -->
+        <script src="./corano/assets/js/plugins/slick.min.js"></script>
+        <!-- Countdown JS -->
+        <script src="./corano/assets/js/plugins/countdown.min.js"></script>
+        <!-- Nice Select JS -->
+        <script src="./corano/assets/js/plugins/nice-select.min.js"></script>
+        <!-- jquery UI JS -->
+        <script src="./corano/assets/js/plugins/jqueryui.min.js"></script>
+        <!-- Image zoom JS -->
+        <script src="./corano/assets/js/plugins/image-zoom.min.js"></script>
+        <!-- Images loaded JS -->
+        <script src="./corano/assets/js/plugins/imagesloaded.pkgd.min.js"></script>
+        <!-- mail-chimp active js -->
+        <script src="./corano/assets/js/plugins/ajaxchimp.js"></script>
+        <!-- contact form dynamic js -->
+        <script src="./corano/assets/js/plugins/ajax-mail.js"></script>
+        <!-- google map api -->
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCfmCVTjRI007pC1Yk2o2d_EhgkjTsFVN8"></script>
+        <!-- google map active js -->
+        <script src="./corano/assets/js/plugins/google-map.js"></script>
+        <!-- Main JS -->
+        <script src="./corano/assets/js/main.js"></script>
     </body>
+
     </html>
-<?php include "./include/footerClient.php"; ?>
+    <?php include "./include/footerClient.php"; ?>
